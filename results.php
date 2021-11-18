bf7bbd48ce4499b28c28396e15d5018b09c3ed45
maralexv <scarvesfamily@yahoo.com>
2021-11-18 07:20:55 +0100

    slight updates to results.php

--- results.php
+++ results.php
@@ -3,19 +3,27 @@
 <head> 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Santa Letter - All Results</title>
+    <title>Santa Letter - All Wishes</title>
 
     <?php require 'header.php';?>
 
 </head>
 
 <body>
-<h1>RESULTS</h1>
 
-<div id="allnotes" style="display: none;"><span></span></div>
-<div id="wishes" style="display: none;"></div>
+    <div id="allnotes" style="display: block;">
+        <h1 style="text-align: center;">All Christmas wish lists:</h1>
+    </div>
 
-<footer>Copyright scarvesfamily&copy; <?php echo date("Y");?></footer><br>
+    <div id="wishes" style="display: block;">
+        <?php
+
+
+
+        ?>
+    </div>
+
+    <footer>Copyright scarvesfamily&copy; <?php echo date("Y");?></footer><br>
 
 </body>
 </html>
